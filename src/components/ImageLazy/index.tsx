2ac4e5116ede143e136e22f861d16d96dc40a7dd
stock <1378538318@qq.com>
2022-07-12 23:39:48 +0800

    懒加载

--- src/components/ImageLazy/index.tsx
+++ src/components/ImageLazy/index.tsx
@@ -67,26 +67,26 @@ const ImageLazy: FC<ImagelazyProps> = ({
 
     return (
         <>
-            {loadFail ? (
-                <div className={classnames(boxClassName)}>
+            <LazyLoad
+                className={classnames(boxClassName)}
+                placeholder={
                     <Placeholder
                         iconClasssName={iconClasssName}
                         boxIconClassName={boxIconClassName}
                     />
-                </div>
-            ) : (
-                <LazyLoad
-                    className={classnames(boxClassName)}
-                    placeholder={
+                }
+            >
+                {loadFail ? (
+                    <div className={classnames(boxClassName)}>
                         <Placeholder
                             iconClasssName={iconClasssName}
                             boxIconClassName={boxIconClassName}
                         />
-                    }
-                >
+                    </div>
+                ) : (
                     <img src={src} alt={alt} className={imageClassName} />
-                </LazyLoad>
-            )}
+                )}
+            </LazyLoad>
         </>
     );
 };
